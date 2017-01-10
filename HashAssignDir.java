1116740a9bdaaea61aa7d5c15e45dfeeb4c40043
hduser <hduser@gho10.(none)>
2017-01-10 18:38:57 -0500

    Dir wise partitioner counting.

---
+++ HashAssignDir.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class HashAssignDir {
+
+    public static int getPartition(String key, int numReduceTasks) {
+        return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+
+    }
+
+    public static void main (String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the file path: ");
+        String file = scanner.next();
+        System.out.print("Enter the number of reducers: ");
+        int rnum = Integer.parseInt(scanner.next());
+
+        // File file = new File(filepath);
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String key;
+            int hashvalue;
+            int[] assignment = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+            for(String line; (line = br.readLine()) != null; ) {
+                // process the line.
+                if (line.contains(" ")) {
+	           String[] splits = line.split(" ");
+		   key = splits[0];
+		// key = line.substring(0,10);
+                // System.out.println("Key is: " + key);
+                   hashvalue = getPartition(key,rnum);
+                   assignment[hashvalue] += 1;
+                // System.out.println("Hashed to reducer: " + hashvalue);
+            	}
+	    }
+            for (int element: assignment) {
+                System.out.print(element+",");
+            }
+            // line is not visible here.
+        } catch(IOException ie) {
+            ie.printStackTrace();
+        }
+
+        System.out.println("Finished!");
+    }
+
+}
