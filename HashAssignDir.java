3ca94a44b1eb085cb06b28242267255edf6fc3f5
Xiao Meng <meng14fall@gmail.com>
2017-01-10 19:00:13 -0500

    Implemented HashAssignDir.

--- HashAssignDir.java
+++ HashAssignDir.java
@@ -11,35 +11,42 @@ public class HashAssignDir {
     public static void main (String[] args) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the file path: ");
+        System.out.print("Enter the dir path: ");
         String file = scanner.next();
         System.out.print("Enter the number of reducers: ");
         int rnum = Integer.parseInt(scanner.next());
 
         // File file = new File(filepath);
-        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String key;
-            int hashvalue;
-            int[] assignment = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        File dir = new File(file);
+        File[] directoryListing = dir.listFiles();
 
-            for(String line; (line = br.readLine()) != null; ) {
-                // process the line.
-                if (line.contains(" ")) {
-	           String[] splits = line.split(" ");
-		   key = splits[0];
-		// key = line.substring(0,10);
-                // System.out.println("Key is: " + key);
-                   hashvalue = getPartition(key,rnum);
-                   assignment[hashvalue] += 1;
-                // System.out.println("Hashed to reducer: " + hashvalue);
-            	}
-	    }
-            for (int element: assignment) {
-                System.out.print(element+",");
+        if (directoryListing != null) {
+            for (File child : directoryListing) {
+                try(BufferedReader br = new BufferedReader(new FileReader(child))) {
+                    String key;
+                    int hashvalue;
+                    int[] assignment = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+                    for (String line; (line = br.readLine()) != null; ) {
+                        // process the line.
+                        if (line.contains(" ")) {
+                            String[] splits = line.split(" ");
+                            key = splits[0];
+                            // key = line.substring(0,10);
+                            // System.out.println("Key is: " + key);
+                            hashvalue = getPartition(key, rnum);
+                            assignment[hashvalue] += 1;
+                            // System.out.println("Hashed to reducer: " + hashvalue);
+                        }
+                    }
+                    for (int element : assignment) {
+                        System.out.print(element + ",");
+                    }
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
             }
             // line is not visible here.
-        } catch(IOException ie) {
-            ie.printStackTrace();
         }
 
         System.out.println("Finished!");
