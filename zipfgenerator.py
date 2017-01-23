fb80e8173c919bebd070a62bed7b1cbc2e2b4a1e
Xiao Meng <meng14fall@gmail.com>
2017-01-23 02:59:14 -0500

    New heuristic and zipf data generator.

---
+++ zipfgenerator.py
@@ -0,0 +1,30 @@
+import random
+import bisect
+import math
+
+class ZipfGenerator:
+
+    def __init__(self, n, alpha):
+        # Calculate Zeta values from 1 to n:
+        tmp = [1. / (math.pow(float(i), alpha)) for i in range(1, n+1)]
+        zeta = reduce(lambda sums, x: sums + [sums[-1] + x], tmp, [0])
+
+        # Store the translation map:
+        self.distMap = [x / zeta[-1] for x in zeta]
+
+    def next(self):
+        # Take a uniform 0-1 pseudo-random value:
+        u = random.random()
+
+        # Translate the Zipf variable:
+        return bisect.bisect(self.distMap, u) - 1
+
+n = int(raw_input("Number of records:"))
+alpha = int(raw_input("Value of alpha:"))
+
+generator = ZipfGenerator(n, alpha)
+
+for i in range(n):
+    print generator.next() + 1000000000
+
+
